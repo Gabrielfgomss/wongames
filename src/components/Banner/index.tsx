cef04688205bf2d41363c988d6f49d4509d17357
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-08 12:31:01 -0300

    feature(Banner):  creating the banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import Button from "../Button"
+import * as S from "./styles"
+
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Banner = ({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+}: BannerProps) => (
+  <S.Wrapper>
+    <S.ImageWrapper>
+      <Image src={img} alt={title} layout="fill" objectFit="cover" />
+    </S.ImageWrapper>
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+      <Button as="a" href={buttonLink} size="large">
+        {buttonLabel}
+      </Button>
+    </S.Caption>
+  </S.Wrapper>
+)
+
+export default Banner
