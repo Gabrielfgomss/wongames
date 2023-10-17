ccc85e813a2d0b6baf679d8fca46dad69e50ac3a
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 09:42:24 -0300

    feature(GameInfo): creating the component

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,20 @@
+import { StoryFn, Meta } from "@storybook/react"
+import GameInfo, { GameInfoProps } from "."
+import mockGame from "./mock"
+
+export default {
+  title: "GameInfo",
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+  args: mockGame,
+} as Meta
+
+export const Default: StoryFn<GameInfoProps> = (args) => (
+  <div style={{ maxWidth: "144rem", margin: "auto", padding: "1.5rem" }}>
+    <GameInfo {...args} />
+  </div>
+)
