c5f196ba1bf1e7ccc01e1caf541e4974fba8eda7
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 12:17:05 -0300

    feature(TextContent): creating the component

---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,18 @@
+import { StoryFn, Meta } from "@storybook/react/"
+import TextContent, { TextContentProps } from "."
+import textMock from "./mock"
+
+export default {
+  title: "TextContent",
+  component: TextContent,
+  args: textMock,
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta<TextContentProps>
+
+export const Default: StoryFn<TextContentProps> = (args) => (
+  <TextContent {...args} />
+)
