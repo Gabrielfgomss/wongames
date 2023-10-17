c40c6bc7ec54659bedbc41a64c04842802d556e3
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 09:41:38 -0300

    feature(GameDetails): creating the component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,29 @@
+import { StoryFn, Meta } from "@storybook/react"
+import GameDetails, { GameDetailsProps } from "."
+
+export default {
+  title: "Game/GameDetails",
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+  args: {
+    platforms: ["windows", "linux", "mac"],
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: "inline-check",
+        options: ["windows", "linux", "mac"],
+      },
+    },
+  },
+} as Meta
+
+export const Default: StoryFn<GameDetailsProps> = (args) => (
+  <div style={{ maxWidth: "130rem", margin: "0 auto" }}>
+    <GameDetails {...args} />
+  </div>
+)
