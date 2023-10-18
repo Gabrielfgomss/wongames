0c45a2a46f6fb35329baed3eeccaf6293077a585
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 23:38:02 -0300

    feature(Wishlist): creating page

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,18 @@
+import Wishlist, { WishlistTemplateProps } from "../../templates/Wishlist"
+import gamesMock from "../../components/GameCardSlider/mock"
+import highlightMock from "../../components/Highlight/mock"
+
+export async function getProps() {
+  return {
+    props: {
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighlight: highlightMock,
+      games: gamesMock,
+    },
+  }
+}
+
+export default async function WishlistPage() {
+  const { props }: { props: WishlistTemplateProps } = await getProps()
+  return <Wishlist {...props} />
+}
