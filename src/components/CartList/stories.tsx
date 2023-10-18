37a40c61b55f97d10f280e3ce0cd82d3ae84ac5b
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 23:29:25 -0300

    feature(CartList): creating the component

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,29 @@
+import { StoryFn, Meta } from "@storybook/react"
+import CartList, { CartListProps } from "."
+
+import mockItems from "./mock"
+
+export default {
+  title: "CartList",
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: "R$ 330,00",
+  },
+  argTypes: {
+    items: {
+      type: undefined,
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta
+
+export const Default: StoryFn<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
