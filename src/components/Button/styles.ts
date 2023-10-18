7f3e1c3f2ac845f491bd03e2347c74888662370a
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-17 23:32:36 -0300

    feature(Button): adding disabled state

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -45,10 +45,16 @@ const wrapperModifiers = {
       }
     }
   `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
+  `,
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -69,5 +75,6 @@ export const Wrapper = styled.button<WrapperProps>`
     ${!!fullWidth && wrapperModifiers.fullWidth()};
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
     ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
   `}
 `
