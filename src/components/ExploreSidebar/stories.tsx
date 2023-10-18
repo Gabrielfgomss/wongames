ba3c6f05206a307b03897e20c6897b2d6f582241
Gabriel Gomes <gabrielfgomss@gmail.com>
2023-10-18 16:26:30 -0300

    feature(ExploreSidebar): creating the component

---
+++ src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,33 @@
+import { StoryFn, Meta } from "@storybook/react"
+import ExploreSidebar, { ExploreSidebarProps } from "."
+
+import items from "./mock"
+
+export default {
+  title: "ExploreSidebar",
+  component: ExploreSidebar,
+  args: {
+    items,
+  },
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta
+
+export const Default: StoryFn<ExploreSidebarProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    <ExploreSidebar {...args} />
+  </div>
+)
+
+export const WithInitialValues: StoryFn<ExploreSidebarProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 320 }}>
+    <ExploreSidebar
+      {...args}
+      initialValues={{ windows: true, sort_by: "low-to-high" }}
+    />
+  </div>
+)
