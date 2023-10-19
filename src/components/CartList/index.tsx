6412ce3c729f9ae10677be02c83bbb453ff68d57
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-19 18:44:31 -0300

    feature: adding when empty and with button

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,21 +1,41 @@
+import Link from "next/link"
 import GameItem, { GameItemProps } from "../../components/GameItem"
-
+import Empty from "../../components/Empty"
 import * as S from "./styles"
+import Button from "../Button"
 
 export type CartListProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
+  hasButton?: boolean
 }
 
-const CartList = ({ items, total }: CartListProps) => (
-  <S.Wrapper>
-    {items.map((item) => (
-      <GameItem key={item.title} {...item} />
-    ))}
+const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
+  <S.Wrapper isEmpty={!items.length}>
+    {items.length ? (
+      <>
+        {items.map((item) => (
+          <GameItem key={item.title} {...item} />
+        ))}
 
-    <S.Footer>
-      Total <S.Total>{total}</S.Total>
-    </S.Footer>
+        <S.Footer>
+          {!hasButton && <span>Total:</span>}
+          <S.Total>{total}</S.Total>
+
+          {hasButton && (
+            <Link href="/cart">
+              <Button as="span">Buy it now</Button>
+            </Link>
+          )}
+        </S.Footer>
+      </>
+    ) : (
+      <Empty
+        title="Your cart is empty"
+        description="Go back to the store and explore great games and offers."
+        hasLink
+      />
+    )}
   </S.Wrapper>
 )
 
