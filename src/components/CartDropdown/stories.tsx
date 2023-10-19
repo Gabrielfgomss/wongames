6b79389dd1f4e36603a3c59dde84ba4bf9bde5c6
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-19 18:35:25 -0300

    feature(CartDropDown): creating the component

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,29 @@
+import { StoryFn, Meta } from "@storybook/react"
+import CartDropdown, { CartDropdownProps } from "."
+import items from "../../components/CartList/mock"
+
+export default {
+  title: "CartDropdown",
+  component: CartDropdown,
+  args: {
+    items,
+    total: "R$ 300,00",
+  },
+  parameters: {
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta
+
+export const Default: StoryFn<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: "98%", display: "flex", justifyContent: "flex-end" }}>
+    <CartDropdown {...args} />
+  </div>
+)
+
+export const Empty: StoryFn<CartDropdownProps> = () => (
+  <div style={{ maxWidth: "98%", display: "flex", justifyContent: "flex-end" }}>
+    <CartDropdown />
+  </div>
+)
