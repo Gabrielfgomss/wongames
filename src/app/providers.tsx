47308d86a8b1c99f6b316585dedb456737f42cb3
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-10-19 22:38:10 -0300

    feature: adding the apollo client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,21 @@
 
 import { GlobalStyles } from "@/styles/global"
 import theme from "@/styles/theme"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import { PropsWithChildren } from "react"
 import { ThemeProvider } from "styled-components"
 
 export function Providers({ children }: PropsWithChildren) {
+  const client = new ApolloClient({
+    uri: "http://localhost:1337/graphql",
+    cache: new InMemoryCache(),
+  })
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
