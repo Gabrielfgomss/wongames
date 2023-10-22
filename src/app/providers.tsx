9fe47be61f4267263f51882178207e20e866a543
Gabriel Gomes <gabrielfgomss@gmail.com>
2023-10-22 16:29:03 -0300

    fix: refactoring apollo client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,21 +2,14 @@
 
 import { GlobalStyles } from "@/styles/global"
 import theme from "@/styles/theme"
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import { PropsWithChildren } from "react"
 import { ThemeProvider } from "styled-components"
 
 export function Providers({ children }: PropsWithChildren) {
-  const client = new ApolloClient({
-    uri: "http://localhost:1337/graphql",
-    cache: new InMemoryCache(),
-  })
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        {children}
-      </ThemeProvider>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
   )
 }
