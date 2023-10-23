b21ed2b148dd4b3be3c28fc75caa3b2a812c7540
Gabriel Gomes <gabrielfgomss@gmail.com>
2023-10-23 18:37:32 -0300

    feature: fatching data and show more

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,25 +1,28 @@
 "use client"
 
 import { KeyboardArrowDown as ArrowDown } from "@styled-icons/material-outlined/KeyboardArrowDown"
-
 import ExploreSidebar, { ItemProps } from "../../components/ExploreSidebar"
 import GameCard, { GameCardProps } from "../../components/GameCard"
 import { Grid } from "../../components/Grid"
-
 import * as S from "./styles"
+import { useQueryGames } from "@/graphql/queries/games"
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, fetchMore } = useQueryGames({
+    variables: { limit: 15 },
+  })
+  console.log(data)
   const handleFilter = () => {
     return
   }
 
   const handleShowMore = () => {
-    return
+    fetchMore({ variables: { limit: 15, start: data?.games.data.length } })
   }
 
   return (
@@ -28,8 +31,15 @@ const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
 
       <section>
         <Grid>
-          {games.map((item) => (
-            <GameCard key={item.title} {...item} />
+          {data?.games.data.map((game) => (
+            <GameCard
+              key={game.attributes.slug}
+              title={game.attributes.name}
+              slug={game.attributes.slug}
+              developer={game.attributes.developers.data[0].attributes.name}
+              img={`http://localhost:1337${game.attributes.cover?.data?.attributes.url}`}
+              price={game.attributes.price}
+            />
           ))}
         </Grid>
 
