f1be0f40f94f55138141d463d76a6252c6e096fe
Gabriel Gomes <gabrielfgomss@gmail.com>
2023-10-31 01:44:20 -0300

    feature: validation for inputs

---
+++ src/utils/validations/index.ts
@@ -0,0 +1,61 @@
+import Joi from "joi"
+
+const fieldsValidations = {
+  username: Joi.string().min(5).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  confirm_password: Joi.string()
+    .valid(Joi.ref("password"))
+    .required()
+    .messages({ "any.only": "confirm password does not match with password" }),
+}
+
+export type FieldErrors = {
+  [key: string]: string
+}
+
+function getFieldErrors(objError: Joi.ValidationResult) {
+  const errors: FieldErrors = {}
+
+  if (objError.error) {
+    objError.error.details.forEach((err) => {
+      errors[err.path.join(".")] = err.message
+    })
+  }
+
+  return errors
+}
+
+export function signUpValidate(values) {
+  const schema = Joi.object(fieldsValidations)
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+export function signInValidate(values) {
+  const { email, password } = fieldsValidations
+  const schema = Joi.object({ email, password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+export function forgotValidate(values) {
+  const { email } = fieldsValidations
+  const schema = Joi.object({ email })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+type ResetValidateParams = {
+  password: string
+  confirm_password: string
+}
+
+export function resetValidate(values: ResetValidateParams) {
+  const { password, confirm_password } = fieldsValidations
+  const schema = Joi.object({ password, confirm_password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
