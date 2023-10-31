95547bbb99f5e2f963e38a54fb1e0cfbfb63704b
Gabriel Gomes <gabrielfgomss@gmail.com>
2023-10-31 01:55:57 -0300

    fix: refactoring apollo client for SSR

--- src/app/(dashboard)/wishlist/page.tsx
+++ src/app/(dashboard)/wishlist/page.tsx
@@ -1,10 +1,20 @@
 import Wishlist from "@/templates/Wishlist"
-import { getClient } from "@/lib/client"
-import { QUERY_RECOMMENDED } from "@/graphql/queries/recommended"
+import { initializeApolloServer } from "@/lib/client"
+import { QUERY_RECOMMENDED } from "../../../graphql/queries/recommended"
 import { gamesMapper, highlightMapper } from "@/utils/mappers"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 export default async function WishlistPage() {
-  const { data } = await getClient().query({ query: QUERY_RECOMMENDED })
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user) {
+    redirect("/sign-in")
+  }
+  const token = session?.user?.jwt
+  const { data } = await initializeApolloServer(token)().query({
+    query: QUERY_RECOMMENDED,
+  })
   const recommendedTitle = data?.recommended?.data?.attributes.section.title
   const recommendedGames = gamesMapper(
     data?.recommended?.data?.attributes.section.games.data,
